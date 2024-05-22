31e8ce1310ba93c24311139c12749c85e245b871
Lucas Raphael <45089900+LucasRaphaelM@users.noreply.github.com>
2024-05-22 15:40:17 -0300

    Landpage Criada
    landpage criada em html, css e js, agora só colocar no springboot

---
+++ LandPage/functions.js
@@ -0,0 +1,9 @@
+function mudavideo1(){
+    document.getElementById("videopt").style.display="block";
+    document.getElementById("videoen").style.display="none";
+}
+
+function mudavideo2(){
+    document.getElementById("videopt").style.display="none";
+    document.getElementById("videoen").style.display="block";
+}
